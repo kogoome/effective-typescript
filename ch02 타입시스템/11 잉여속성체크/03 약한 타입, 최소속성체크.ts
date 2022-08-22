5b8738281ceba609c9dd84131852c97eafcdad4a
wistaria <kogoome@kakao.com>
2022-08-22 09:37:35 +0900

    수정

--- ch02 타입시스템/11 잉여속성체크/03 약한 타입, 최소속성체크.ts
+++ ch02 타입시스템/11 잉여속성체크/03 약한 타입, 최소속성체크.ts
@@ -4,9 +4,21 @@
 // * 공통속성이 존재하지 않고 다른속성이 존재하면 오류를 나타냄
 // * 빈객체는 속성 자체가 부재하기 때문에 공통속성체크 회피 그냥 옵셔널만 처리해도 문제가 없음.
 
-
 /* 
 !심화학습
+약한타입 판단함수
+function isWeakType(type: Type): boolean {
+    if (type.flags & TypeFlags.Object) {
+        const resolved = resolveStructuredTypeMembers(type as ObjectType);
+
+        return resolved.callSignatures.length === 0 && resolved.constructSignatures.length === 0 && resolved.indexInfos.length === 0 &&
+            resolved.properties.length > 0 && every(resolved.properties, p => !!(p.flags & SymbolFlags.Optional));
+    }
+    if (type.flags & TypeFlags.Intersection) {
+        return every((type as IntersectionType).types, isWeakType);
+    }
+    return false;
+}
 공통속성체크 함수
 function hasCommonProperties(source: Type, target: Type, isComparingJsxAttributes: boolean) {
     소스가 빈객체라면 포문이 안돌아가고 트루를 리턴할수 없음.
@@ -41,11 +53,30 @@ const isPerformingCommonPropertyChecks =
       && target.flags & (TypeFlags.Object | TypeFlags.Intersection) 
 *     && isWeakType(target) 위크타입을 만족하고
 *     && (getPropertiesOfType(source).length > 0 || typeHasCallOrConstructSignatures(source))
-*        프로퍼티의 랭스가 1이상(속성이 존재)이거나 생성자시그니처가 있는경우
+*        프로퍼티의 랭스가 1이상(속성이 존재)이거나 콜시그니처 혹은 생성자시그니처가 있는경우
+
+? 콜시그니처란
+const fn = (x:number)=>x>1
+이라는 함수에서
+fn.title = 'black'
+을 주면 함수인데 프로퍼티를 가지게 된다. (자바스크립트 함수는 일급객체)
+이런 함수의 타입을 정의할때
+
+interface CallSignature {
+    title:string
+    (x: number):boolean;
+} 
+의 형식을 사용하는데 이런 함수타입의 정의를 콜시그니처라 한다.
+(프로퍼티가 없어도 콜시그니처라 부른다.)
+
+? 생성자시그니처란
+interface StringConstructable {
+    new(n: string): ComesFromString;
+}
+콜시그니처와 유사하지만 매개변수 앞에 new 키워드가 있을때를 말한다.
 
 ! 위크타입이고, 속성을 for문 돌려서 공통속성이 없을 때 오류리포트 해줌
 */
-
 
 interface LineChartOptions {
 	logscale?: boolean
@@ -61,7 +92,7 @@ const o2: LineChartOptions = { a: 122, logscale: true } // 속성오류
 const opts3 = {}
 const o3: LineChartOptions = opts3 // 빈객체와 할당가능성
 // NOTE 이론적으로 빈객체가 할당 가능하면 모든 객체가 허용되야 함.
-// 이런 사태를 방지하기 위해 공통속성이 있는지 별도로 체크
+// 이런 사태를 방지하기 위해 속성이 있고 위크타입인경우(위에 발동조건있음) 공통속성이 있는지 별도로 체크
 const opts4 = { a: 122 } // 속성이 존재하나 공통속성이 부재. 오류
 const o4: LineChartOptions = opts4
 const opts5 = { a: 122, logscale: true }
@@ -85,16 +116,16 @@ let d2: C = d
 
 type NoPropertySet = {} // 빈객체 - 수퍼셋
 type PropertySet = { a: number } // 빈객체의 부분집합
-type EveryStringKeySet = { [key:string]:unknown}
+type EveryStringKeySet = { [key: string]: unknown }
 
 let e: PropertySet = { a: 1 }
-let e2: NoPropertySet = e 
+let e2: NoPropertySet = e
 
 type NoPropertySet = {} // 빈객체 - 수퍼셋
 type PropertySet = { a: number } // 빈객체의 부분집합
 type EveryStringKeySet = { [key: string]: unknown }
 
-let e3: EveryStringKeySet = { a:1, b:'2', c:true, d:null}
+let e3: EveryStringKeySet = { a: 1, b: '2', c: true, d: null }
 let e4: NoPropertySet = e3 // 빈객체를 할당가능하다면 모든 객체타입을 매개한 변수 또한 할당가능
 
 export default {}
